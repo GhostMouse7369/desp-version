e305a8115a04bc9ed5be8768d67e076c05e08412
laoshuzi <215779374@qq.com>
2021-07-08 17:17:52 +0800

    [update]android构建配置

---
+++ buildSrc/src/main/kotlin/top/laoshuzi/dependencies/config/AndroidBuildConfig.kt
@@ -0,0 +1,18 @@
+package top.laoshuzi.dependencies.config
+
+/**
+ * Created by mouse on 2019/6/15.
+ */
+object AndroidBuildConfig {
+
+    val application_id = "top.laoshuzi.test"
+
+    val build_tools = "30.0.2"
+    val min_sdk: Int = 19
+    val target_sdk: Int = 30
+    val version_code: Int = 1
+    val version_name = "0.1.0"
+    val test_instrumentation_runner = "android.support.test.runner.AndroidJUnitRunner"
+    val consumer_file = "consumer-rules.pro"
+    val proguard_file = "proguard-rules.pro"
+}
